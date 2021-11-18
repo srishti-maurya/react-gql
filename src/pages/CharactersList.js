389928bf4df4730753a1dfc68d45688b345cc7fc
srishti-maurya <srishti22maurya@gmail.com>
2021-11-18 11:34:03 +0530

    query hooks

--- src/pages/CharactersList.js
+++ src/pages/CharactersList.js
@@ -1,23 +1,9 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
 import "./CharacterList.css";
-
-const GET_CHARACTERS = gql`
-  query {
-    characters {
-      results {
-        id
-        name
-        image
-      }
-    }
-  }
-`;
+import { useCharacters } from "../hooks/useCharacters";
 
 export default function CharactersList() {
-  const { error, data, loading } = useQuery(GET_CHARACTERS);
-
-  //   console.log({ error, loading, data });
+  const { error, loading, data } = useCharacters();
   if (loading) return <div>spinner</div>;
   if (error) return <div>something went wrong</div>;
   return (
