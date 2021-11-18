4fb22f083f4052a57b2077263891d19067ed1b93
srishti-maurya <srishti22maurya@gmail.com>
2021-11-18 11:28:05 +0530

    using queries

--- src/pages/CharactersList.js
+++ src/pages/CharactersList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
+import "./CharacterList.css";
 
 const GET_CHARACTERS = gql`
   query {
@@ -16,8 +17,19 @@ const GET_CHARACTERS = gql`
 export default function CharactersList() {
   const { error, data, loading } = useQuery(GET_CHARACTERS);
 
-  console.log({ error, loading, data });
+  //   console.log({ error, loading, data });
   if (loading) return <div>spinner</div>;
   if (error) return <div>something went wrong</div>;
-  return <div></div>;
+  return (
+    <div className="CharacterList">
+      {data.characters.results.map((character) => {
+        return (
+          <div>
+            <img src={character.image} alt="character-img" />
+            <h2>{character.name}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
